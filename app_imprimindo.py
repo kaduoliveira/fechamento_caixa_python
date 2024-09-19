a3d7b6ff39b06e9968e9efa81c16504d820fd4cc
Kadu Oliveira <carloseduardo_al@hotmail.com>
2024-09-19 05:23:52 -0300

    função de imprimir

--- app_imprimindo.py
+++ app_imprimindo.py
@@ -2,10 +2,11 @@ from datetime import datetime
 import os
 import win32print
 import win32api
-import subprocess
 
+# Coleta da data atual
 data_atual = datetime.now()
 
+# Exibição no terminal
 print()
 print("Seja bem-vindo ao".center(35))
 print("-------------------------------------")
@@ -15,6 +16,7 @@ print(data_atual.strftime("%d/%m/%Y -- %H:%M:%S").center(35))
 print()
 func_caixa = str(input("Responsável pelo Caixa: "))
 
+# Seleção do responsável pelo caixa
 match func_caixa:
     case '1':
         print("Michelly".upper())
@@ -25,8 +27,8 @@ match func_caixa:
 
 func_caixa = func_caixa.capitalize()
 
+# Variáveis para os valores do fechamento
 cheque = 0
-
 print("      \nDigitando o primeiro caixa:\n ")
 dinheiro = float(input("Vendas em DINHEIRO: "))
 dinheiro = round(dinheiro, 2)
@@ -36,32 +38,34 @@ cartao_credito = float(input("Vendas em CARTÃO CRÉDITO: "))
 cartao_credito = round(cartao_credito, 2)
 cheque = float(input("Vendas em CHEQUE: "))
 cheque = round(cheque, 2)
-despesa = float(input("Totals das DESPESAS: "))
+despesa = float(input("Total das DESPESAS: "))
 despesa = round(despesa, 2)
 
+# Limpa o terminal
 os.system('cls')
-print("----------------------------------------------")
-print("----_F_E_C_H_A_M_E_N_T_O__D_O__C_A_I_X_A_-----")
-print("----------------------------------------------")
-print("---------------------VALOR--------------------")
-print("DINHEIRO       |",f" {dinheiro} |".rjust(10))
-print("Cartão Débito  |",f" {cartao_debito} |".rjust(10))
-print("Cartão Crédito |",f" {cartao_credito} |".rjust(10))
-print("Cheque         |",f" {cheque} |".rjust(10))
-print("----------------------------------------------")
-total1 = dinheiro + cartao_debito + cartao_credito + cheque
-print(f"Total = {total1:.2f}")
-print(f"Despesas = {despesa:.2f}")
-print()
-print("----------------------------------------------")
-resultado = total1 - despesa
-print(f" RESULTADO = R$ {resultado:.2f}")
-print("----------------------------------------------")
 
+# Cálculos
+total1 = dinheiro + cartao_debito + cartao_credito + cheque
+resultado = total1 - despesa
+
+# Armazenar a saída em um arquivo de texto
 file_name = "saida_cmd.txt"
 with open(file_name, "w") as f:
-    subprocess.run(stdout=f, shell=True)
+    f.write("----------------------------------------------\n")
+    f.write("----_F_E_C_H_A_M_E_N_T_O__D_O__C_A_I_X_A_-----\n")
+    f.write("----------------------------------------------\n")
+    f.write("---------------------VALOR--------------------\n")
+    f.write(f"DINHEIRO       | {dinheiro} |\n")
+    f.write(f"Cartão Débito  | {cartao_debito} |\n")
+    f.write(f"Cartão Crédito | {cartao_credito} |\n")
+    f.write(f"Cheque         | {cheque} |\n")
+    f.write("----------------------------------------------\n")
+    f.write(f"Total = {total1:.2f}\n")
+    f.write(f"Despesas = {despesa:.2f}\n")
+    f.write("----------------------------------------------\n")
+    f.write(f"RESULTADO = R$ {resultado:.2f}\n")
+    f.write("----------------------------------------------\n")
 
+# Envio do arquivo para a impressora
 printer_name = win32print.GetDefaultPrinter()
-file_name = "arquivo.txt"
 win32api.ShellExecute(0, "print", file_name, None, ".", 0)
